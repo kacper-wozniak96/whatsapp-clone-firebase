47fbeb37d9f4daf182f09ef3fd67d7ff95de643d
Kacper Woźniak <starzynek2@gmail.com>
2020-11-08 16:10:12 +0100

    starting point

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDedRPrQ64_c01iAN6Jj1CHmCeweUVPCDw',
+  authDomain: 'whatsapp-firebase-c56be.firebaseapp.com',
+  databaseURL: 'https://whatsapp-firebase-c56be.firebaseio.com',
+  projectId: 'whatsapp-firebase-c56be',
+  storageBucket: 'whatsapp-firebase-c56be.appspot.com',
+  messagingSenderId: '313946524575',
+  appId: '1:313946524575:web:ca7bf21e945e0b633a8baa',
+};
