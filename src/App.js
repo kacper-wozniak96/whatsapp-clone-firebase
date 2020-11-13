3adb865f0c45f19ac318b64061e29de18ea244ec
Kacper Woźniak <starzynek2@gmail.com>
2020-11-13 15:46:06 +0100

    Fix css Sidebar width for mobile

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Chat/Chat';
 
 function App() {
-  // // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  // const vh = window.innerHeight * 0.01;
-  // // Then we set the value in the --vh custom property to the root of the document
-  // document.documentElement.style.setProperty('--vh', `${vh}px`);
 
   const { windowWidth } = useViewport();
-
   const breakpoint = 620;
 
   const adjustingVh = () => {
@@ -29,11 +24,7 @@ function App() {
     return () => window.removeEventListener('resize', adjustingVh);
   }, []);
 
-  // window.addEventListener('resize', () => {
-  //   // We execute the same script as before
-  //   const vh = window.innerHeight * 0.01;
-  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  // });
+
 
   return (
     <div className="app">
