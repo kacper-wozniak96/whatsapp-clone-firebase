40cfeca7e7af4d3aee53ec058533d024f63b9906
Kacper Woźniak <starzynek2@gmail.com>
2020-12-04 12:55:49 +0100

    Add redirect login with Google option

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,25 +2,48 @@ import { Button } from '@material-ui/core';
 import React from 'react';
 import { actionTypes } from '../../contexts/contextUser/reducer';
 import { useStateValue } from '../../contexts/contextUser/UserStateProvider';
+import { useViewport } from '../../contexts/contextViewport';
 import { auth, provider } from '../../firebase';
 import './Login.scss';
 
 export default function Login() {
   // eslint-disable-next-line no-unused-vars
   const [{ user }, dispatch] = useStateValue();
+  // eslint-disable-next-line no-unused-vars
+  const { windowWidth, breakpoint } = useViewport();
 
   const signIn = () => {
+    if (windowWidth > 1200) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) =>
+          // console.log(result)
+          dispatch({
+            type: actionTypes.SET_USER,
+            payload: result.user,
+          })
+        )
+        // eslint-disable-next-line no-alert
+        .catch((error) => alert(error.message));
+    } else {
+      auth.signInWithRedirect(provider);
+    }
+  };
+
+  const initApp = () => {
     auth
-      .signInWithPopup(provider)
+      .getRedirectResult()
       .then((result) =>
-        // console.log(result)
         dispatch({
           type: actionTypes.SET_USER,
           payload: result.user,
         })
       )
-      // eslint-disable-next-line no-alert
       .catch((error) => alert(error.message));
+  };
+
+  window.onload = function () {
+    initApp();
   };
 
   return (
